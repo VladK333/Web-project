cef860ebe3adacb5d1d1100d87321b46311d93ab
vladanaaleksic3 <vladanaaleksic3@gmail.com>
2025-09-25 00:17:02 +0200

    klase aranzmana

---
+++ web/Models/Aranzman.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace projekat.Models
+{
+    public enum TipAranzmana
+    {
+        NocenjeSDoruckom,
+        Polupansion,
+        PunPansion,
+        AllInclusive,
+        NajamApartmana
+    }
+
+    public enum TipPrevoza
+    {
+        Autobus,
+        Avion,
+        AutobusAvion,
+        Individualan,
+        Ostalo
+    }
+
+    public class Aranzman
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public string Naziv { get; set; }
+
+        [Required]
+        public TipAranzmana TipAranzmana { get; set; }
+
+        [Required]
+        public TipPrevoza TipPrevoza { get; set; }
+
+        [Required]
+        public string Lokacija { get; set; }
+
+        [Required]
+        [JsonConverter(typeof(DateParser), "dd/MM/yyyy")]
+        public DateTime DatumPocetka { get; set; }
+
+        [Required]
+        [JsonConverter(typeof(DateParser), "dd/MM/yyyy")]
+        public DateTime DatumZavrsetka { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Maksimalan broj putnika mora biti veći od 0.")]
+        public int MaksimalanBrojPutnika { get; set; }
+
+        [Required]
+        public string Opis { get; set; }
+
+        [Required]
+        public string ProgramPutovanja { get; set; }
+
+        public string PosterAranzmana { get; set; }
+
+        [Required]
+        public long SmestajId { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public Aranzman() { }
+
+        public Aranzman(long id, string naziv, TipAranzmana tipAranzmana, TipPrevoza tipPrevoza, string lokacija, DateTime datumPocetka, DateTime datumZavrsetka, int maksimalanBrojPutnika, string opis, string programPutovanja, string posterAranzmana, long smestajId, bool isDeleted)
+        {
+            Id = id;
+            Naziv = naziv;
+            TipAranzmana = tipAranzmana;
+            TipPrevoza = tipPrevoza;
+            Lokacija = lokacija;
+            DatumPocetka = datumPocetka;
+            DatumZavrsetka = datumZavrsetka;
+            MaksimalanBrojPutnika = maksimalanBrojPutnika;
+            Opis = opis;
+            ProgramPutovanja = programPutovanja;
+            PosterAranzmana = posterAranzmana;
+            SmestajId = smestajId;
+            IsDeleted = isDeleted;
+        }
+    }
+}
---
+++ web/Models/RezervacijaAranzmana.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace projekat.Models
+{
+    public enum StatusRezervacijeAranzmana { AKTIVNA, OTKAZANA }
+
+    public class RezervacijaAranzmana
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public long TuristaId { get; set; } // ID korisnika koji vrši rezervaciju
+
+        [Required]
+        public StatusRezervacijeAranzmana Status { get; set; } = StatusRezervacijeAranzmana.AKTIVNA;
+
+        [Required]
+        public long AranzmanId { get; set; } // ID izabranog aranžmana
+
+        [Required]
+        public long SmestajnaJedinicaId { get; set; } // ID izabrane smeštajne jedinice
+
+        public RezervacijaAranzmana() { }
+
+        public RezervacijaAranzmana(long id, long turistaId, StatusRezervacijeAranzmana status, long aranzmanId, long smestajnaJedinicaId)
+        {
+            Id = id;
+            TuristaId = turistaId;
+            Status = status;
+            AranzmanId = aranzmanId;
+            SmestajnaJedinicaId = smestajnaJedinicaId;
+        }
+    }
+}
