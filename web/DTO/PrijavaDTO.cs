7b4f7fe5b28d4235db3e555334ebe41ae568976c
vladanaaleksic3 <vladanaaleksic3@gmail.com>
2025-09-24 23:19:57 +0200

    DTO za prijavu i registraciju

---
+++ web/DTO/PrijavaDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projekat.DTO
+{
+    public class PrijavaDTO
+    {
+        [Required]
+        public string KorisnickoIme { get; set; }
+
+        [Required]
+        public string Lozinka { get; set; }
+    }
+}
---
+++ web/DTO/RegistracijaDTO.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace projekat.DTO
+{
+    public class RegistracijaDTO
+    {
+        [Required]
+        public string KorisnickoIme { get; set; }
+
+        [Required]
+        public string Lozinka { get; set; }
+
+        [Required]
+        public string Ime { get; set; }
+
+        [Required]
+        public string Prezime { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public DateTime DatumRodjenja { get; set; }
+
+        [Required]
+        public string Pol { get; set; }
+
+        public RegistracijaDTO() { }
+    }
+}
