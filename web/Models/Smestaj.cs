aed6dd04f47ddbd2969078d200ce14f184d7f37c
vladanaaleksic3 <vladanaaleksic3@gmail.com>
2025-09-24 23:52:28 +0200

    klase za smestaj

---
+++ web/Models/Smestaj.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace projekat.Models
+{
+    public enum TipSmestaja
+    {
+        Hotel,
+        Motel,
+        Vila
+    }
+
+    public class Smestaj
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public TipSmestaja Tip { get; set; }
+
+        [Required]
+        public string Naziv { get; set; }
+
+        public int BrojZvezdica { get; set; }
+
+        public bool ImaBazen { get; set; }
+
+        public bool ImaSpaCentar { get; set; }
+
+        public bool PrilagodjenoZaInvaliditet { get; set; }
+
+        public bool ImaWifi { get; set; }
+
+        [Required]
+        public List<long> SmestajneJedinice { get; set; } = new List<long>();
+
+        public Smestaj() { }
+
+        public Smestaj(long id, TipSmestaja tip, string naziv, int brojZvezdica, bool imaBazen, bool imaSpaCentar, bool prilagodjenoZaInvaliditet, bool imaWifi, List<long> smestajneJedinice)
+        {
+            Id = id;
+            Tip = tip;
+            Naziv = naziv;
+            BrojZvezdica = brojZvezdica;
+            ImaBazen = imaBazen;
+            ImaSpaCentar = imaSpaCentar;
+            PrilagodjenoZaInvaliditet = prilagodjenoZaInvaliditet;
+            ImaWifi = imaWifi;
+            SmestajneJedinice = smestajneJedinice;
+        }
+    }
+}
---
+++ web/Models/SmestajnaJedinica.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace projekat.Models
+{
+    public class SmestajnaJedinica
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public int DozvoljenBrojGostiju { get; set; }
+
+        public bool DozvoljenBoravakKucnimLjubimcima { get; set; }
+
+        [Required]
+        public decimal Cena { get; set; }
+
+        public SmestajnaJedinica() { }
+
+        public SmestajnaJedinica(long id, int dozvoljenBrojGostiju, bool dozvoljenBoravakKucnimLjubimcima, decimal cena)
+        {
+            Id = id;
+            DozvoljenBrojGostiju = dozvoljenBrojGostiju;
+            DozvoljenBoravakKucnimLjubimcima = dozvoljenBoravakKucnimLjubimcima;
+            Cena = cena;
+        }
+    }
+}
