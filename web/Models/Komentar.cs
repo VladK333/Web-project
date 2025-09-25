161b12b97f5bede0664cf1a539bf83c0b33a0116
vladanaaleksic3 <vladanaaleksic3@gmail.com>
2025-09-25 11:38:48 +0200

    kreirana i modifikovana klasa za ostavljanje komentara

---
+++ web/Models/Komentar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace projekat.Models
+{
+    public class Komentar
+    {
+        public long Id { get; set; }
+
+        [Required]
+        public long TuristaId { get; set; } // ID turista koji ostavlja komentar
+
+        [Required]
+        public long SmestajId { get; set; } // ID smeštaja na koji se komentar odnosi
+
+        [Required]
+        public string Tekst { get; set; } 
+
+        [Required]
+        [Range(1, 5)]
+        public int Ocena { get; set; }
+
+        public Komentar() { }
+
+        public Komentar(long id, long turistaId, long smestajId, string tekst, int ocena)
+        {
+            Id = id;
+            TuristaId = turistaId;
+            SmestajId = smestajId;
+            Tekst = tekst;
+            Ocena = ocena;
+        }
+    }
+}
