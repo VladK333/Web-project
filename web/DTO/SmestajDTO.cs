75ce97195b811edd56fdf28e1944f1212e418a99
vladanaaleksic3 <vladanaaleksic3@gmail.com>
2025-09-27 01:45:12 +0200

    povezan nav meni i fje za ispravnost korisnikovih podataka; dto za smestaj i jedinicu

---
+++ web/DTO/SmestajDTO.cs
@@ -0,0 +1,19 @@
+﻿using projekat.Models;
+using System.Collections.Generic;
+
+namespace projekat.DTO
+{
+    public class SmestajDTO
+    {
+        public TipSmestaja Tip { get; set; }
+        public string Naziv { get; set; }
+        public int BrojZvezdica { get; set; }
+        public bool ImaBazen { get; set; }
+        public bool ImaSpaCentar { get; set; }
+        public bool PrilagodjenoZaInvaliditet { get; set; }
+        public bool ImaWifi { get; set; }
+        public List<SmestajnaJedinicaDTO> SmestajneJedinice { get; set; } = new List<SmestajnaJedinicaDTO>();
+
+        public SmestajDTO() { }
+    }
+}
---
+++ web/DTO/SmestajnaJedinicaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace projekat.DTO
+{
+    public class SmestajnaJedinicaDTO
+    {
+        public long Id { get; set; }
+        public int BrojKreveta { get; set; }
+        public decimal Cena { get; set; }
+        public bool DozvoljenBoravakKucnimLjubimcima { get; set; }
+
+        public SmestajnaJedinicaDTO() { }
+    }
+}
